3e115438ac88650b58113eca7e8e94e439063eac
sanatorium <sanatorium@tuta.io>
2018-04-16 12:12:29 +0200

    update

--- install-sanitymn.sh
+++ install-sanitymn.sh
@@ -27,55 +27,55 @@ UNDERLINE='\033[4m'
 ENDCOLOR='\e[m'
 
 # Regular Colors
-BLACK='\[\033[0;30m\]'        # Black
-RED='\[\033[0;31m\]'          # Red
-GREEN='\[\033[0;32m\]'        # Green
-YELLOW='\[\033[0;33m\]'       # Yellow
-BLUE='\[\033[0;34m\]'         # Blue
-PURPLE='\[\033[0;35m\]'       # Purple
-CYAN='\[\033[0;36m\]'         # Cyan
-WHITE='\[\033[0;37m\]'        # White
+BLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+YELLOW='\033[0;33m'       # Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # White
 
 # Bold
-BBlack='\[\033[1;30m\]'       # Black
-BRed='\[\033[1;31m\]'         # Red
-BGreen='\[\033[1;32m\]'       # Green
-BYellow='\[\033[1;33m\]'      # Yellow
-BBlue='\[\033[1;34m\]'        # Blue
-BPurple='\[\033[1;35m\]'      # Purple
-BCyan='\[\033[1;36m\]'        # Cyan
-BWhite='\[\033[1;37m\]'       # White
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
 
 # Underline
-UBlack='\[\033[4;30m\]'       # Black
-URed='\[\033[4;31m\]'         # Red
-UGreen='\[\033[4;32m\]'       # Green
-UYellow='\[\033[4;33m\]'      # Yellow
-UBlue='\[\033[4;34m\]'        # Blue
-UPurple='\[\033[4;35m\]'      # Purple
-UCyan='\[\033[4;36m\]'        # Cyan
-UWhite='\[\033[4;37m\]'       # White
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
 
 # Background
-On_Black='\[\033[40m\]'       # Black
-On_Red='\[\033[41m\]'         # Red
-On_Green='\[\033[42m\]'       # Green
-On_Yellow='\[\033[43m\]'      # Yellow
-On_Blue='\[\033[44m\]'        # Blue
-On_Purple='\[\033[45m\]'      # Purple
-On_Cyan='\[\033[46m\]'        # Cyan
-On_White='\[\033[47m\]'       # White
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
 
 message() {
-	echo -e "${NONE} ${On_Yellow}*** ${BLUE} $1 ${ENDCOLOR}"
+	echo -e "${NONE}${On_Yellow}*** ${BLUE} $1 ${ENDCOLOR}"
 }
 
 messagebig() {
-	echo -e "${On_Black} ${BLUE}"
+	echo -e "${On_Black}${BLUE}"
 	echo -e "********************************************************************"
 	echo -e "********************************************************************"
 	echo -e "***"
-	echo -e "*** ${WHITE} $1 ${BLUE}"
+	echo -e "*** ${WHITE}$1${BLUE}"
 	echo -e "***"
 	echo -e "********************************************************************"
 	echo -e "********************************************************************"
@@ -459,7 +459,7 @@ displayPromptToSendFunds() {
 
 startInstall() {
 	checkForUbuntuVersion
-	createUser
+	#createUser
 	updateAndUpgrade
 	installDependencies
 	installFail2Ban
