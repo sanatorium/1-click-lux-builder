7717b693e209c7b2b9154de4fa7c0b9f06ec667e
sanatorium <>
2018-05-15 20:02:13 +0200

    update

--- install-web.sh
+++ install-web.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+USERNAME=sanitycore
 COINCORE=.sanitycore
 COINPORT=9999
 COINRPCPORT=9998
@@ -12,7 +13,7 @@ cd web
 wget
 wget
 #todo
-(crontab -l 2>/dev/null; echo "* * * * * echo MN Count:  > ~/web/stats.txt; ~/${COINCORE}/${COINCLI} masternode count >> ~/web/stats.txt; ~/${COINCORE}/${COINCLI} getinfo >> ~/web/stats.txt") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * echo MN Count:  > ~/web/stats.txt; /home/${USERNAME}/${COINCORE}/${COINCLI} masternode count >> /home/${USERNAME}/web/stats.txt; /home/${USERNAME}/${COINCORE}/${COINCLI} getinfo >> /home/${USERNAME}/web/stats.txt") | crontab -
 mnip=$(curl -s https://api.ipify.org)
 python3 -m http.server 8000 --bind $mnip 2>/dev/null &
 echo "Your Sanity Masternode Web Server Started!  You can now access your stats page at http://$mnip:8000"
